0d4977569984f1130a348b17d707711d889aa504
beginnerfei <beginnerfei@qq.com>
2023-03-11 10:48:14 +0800

    程序中 +号 的使用

---
+++ Plus.java
@@ -0,0 +1,10 @@
+public class Plus {
+	//编写一个main方法
+	public static void main(String[] args) {
+		System.out.println(100+98);//198
+		System.out.println("100"+98);//10098
+		System.out.println(100+98+"hello");//198hello
+		System.out.println("hello"+100+98);//hello10098
+		
+	}
+}
