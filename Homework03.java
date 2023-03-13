5387f435e64aac4a0dca735f95057e785c3398a8
beginnerfei <beginnerfei@qq.com>
2023-03-13 10:08:07 +0800

    0060本章作业03

---
+++ Homework03.java
@@ -0,0 +1,16 @@
+public class Homework03 {
+	public static void main(String[] args) {
+		String book1 = "天龙八部";
+		String book2 = "天龙八部";
+		System.out.println(book1 + book2);
+		
+		char c1 = '男';
+		char c2 = '女';
+		System.out.println(c1 + c2);
+		
+		double price1 = 123.56;
+		double price2 = 100.11;
+		System.out.println(price1 + price2);
+		
+	}
+}
