b9ee2d4ca89d199cd208d14fee4e8c916f60d785
beginnerfei <beginnerfei@qq.com>
2023-03-14 09:31:33 +0800

    赋值运算符细节

---
+++ AssignOperator.java
@@ -0,0 +1,18 @@
+public class AssignOperator {
+	public static void main(String[] args) {
+		
+		int n1 =10;
+		n1 += 4;
+		System.out.println(n1);
+		
+		n1/= 3;
+		System.out.println(n1);
+		
+		byte b = 3;
+		b += 2;//b=b+2
+		b++;
+		System.out.println(b);
+		
+	}
+}
+	
