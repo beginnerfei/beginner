7a0d897680209188ffb6923c26e8a088f2868d82
beginnerfei <beginnerfei@qq.com>
2023-03-14 09:45:37 +0800

    三元运算符细节

---
+++ TernaryOperatorDetail.java
@@ -0,0 +1,14 @@
+public class TernaryOperatorDetail {
+	public static void main(String[] args) {
+		
+		int a =3;
+		int b = 8;
+		int c = a > b ? a++ : b--;
+		
+		
+		System.out.println("c=" + c);
+		System.out.println("a=" + a);
+		System.out.println("b=" + b);
+		
+	}
+}
