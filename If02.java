c91ede2d7cb2e8f95b64cb700aa60ccd45f5ebc0
beginnerfei <beginnerfei@qq.com>
2023-03-16 15:03:04 +0800

    双分支使用

---
+++ If02.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class If02 {
+	public static void main(String[] args) {
+		
+		Scanner myScanner = new Scanner(System.in);
+		System.out.println("请输入年龄");
+		int age = myScanner.nextInt();
+		if(age > 18) {
+;
+		}else {
+			System.out.println("你的年龄不大，这次放过你了");
+			
+		}
+		System.out.println("程序继续...");
+	}
+}
