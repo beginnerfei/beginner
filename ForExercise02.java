929c5df228579b15d3ac4e236ca3464a94e60730
beginnerfei <beginnerfei@qq.com>
2023-03-17 11:20:43 +0800

    for编程思想2

---
+++ ForExercise02.java
@@ -0,0 +1,12 @@
+public class ForExercise02 {
+	public static void main(String[] args) {
+		int n = 10;
+		for(int i=0; i<=5;i++) {
+			System.out.println(i + "+" + (n-i) + "="+ n );
+			
+			
+		}
+		
+		
+	}
+}
