b14044f65f38ff9762fcb02958d47efbabd602c4
beginnerfei <beginnerfei@qq.com>
2023-03-17 10:42:48 +0800

    for执行流程

---
+++ For01.java
@@ -0,0 +1,13 @@
+public class For01 {
+	public static void main(String[] args) {
+		
+		/*  循环初始化;循环条件;循环变量迭代{
+		循环操作
+		}  */
+		
+		
+		for(int i=1;i<10;i++) {
+			System.out.println("循环语句"+i);
+		}
+	}
+}
