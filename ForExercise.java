9ea06a42bca8cb6475a9ff362ea3a122c5ffc2be
beginnerfei <beginnerfei@qq.com>
2023-03-17 11:12:11 +0800

    for编程思想1

---
+++ ForExercise.java
@@ -0,0 +1,22 @@
+public class ForExercise {
+	public static void main(String[] args) {
+		
+		int count = 0;
+		int sum = 0;
+		int start = 1;
+		int end = 100;
+		int t = 5;
+		
+		
+		for(int i=start; i<=end; i++) {
+			if(i%t==0) {
+				System.out.println("i="+i);
+				count++;
+				sum+=i;
+			}
+			
+		}
+		System.out.println("count=" + count);
+		System.out.println("sum="+sum);
+	}
+}
