3b12fd401a3ea6320e1b21d8b635f3ac67def072
beginnerfei <beginnerfei@qq.com>
2023-03-17 14:54:29 +0800

    while执行流程

---
+++ While01.java
@@ -0,0 +1,12 @@
+public class While01 {
+	public static void main(String[] args) {
+		
+		int i=1;
+		while( i<=10 ) {
+			   System.out.println("你好" + i);
+			   i++;
+		}
+		 System.out.println("推出while循环");
+		 System.out.println(i);
+	}
+}
