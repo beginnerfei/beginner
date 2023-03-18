22cd34f40f4b6b911fd20241b59b86381b51ba3c
beginnerfei <beginnerfei@qq.com>
2023-03-18 15:14:52 +0800

    0149本章作业3

---
+++ Homework06P150.java
@@ -0,0 +1,19 @@
+public class Homework06P150{
+	public static void main(String[] args) {
+		
+		int count = 0;
+		
+		for(int i=1;i<=100;i++) {
+			if(i%5 !=0) {
+				count++;
+				System.out.print(i + "\t");
+				
+				if(count %5 ==0) {
+					System.out.println();
+				}
+			}
+			
+			
+		}
+	}
+}
