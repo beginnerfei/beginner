07e28a0ba233025a255b187c01efb194ad355829
beginnerfei <beginnerfei@qq.com>
2023-03-18 15:26:05 +0800

    0150本章作业4

---
+++ Homework07P151.java
@@ -0,0 +1,15 @@
+public class Homework07P151{
+	public static void main(String[] args) {
+		
+		//'b'='a'+1; 'c'='a'+2
+		
+		for(char c1 = 'a'; c1<='z'; c1++) {
+			System.out.print(c1 +" ");
+		}
+		System.out.println("==========");
+		for(char c1 = 'Z'; c1>='A'; c1--) {
+			System.out.print(c1 +" ");
+		}
+			
+	}
+}
