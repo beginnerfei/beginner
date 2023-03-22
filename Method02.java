9759a63c7f9bae0026c9851d91c8524d3042d3ec
beginnerfei <beginnerfei@qq.com>
2023-03-22 10:24:43 +0800

    方法的妙用

---
+++ Method02.java
@@ -0,0 +1,19 @@
+public class Method02{
+	public static void main(String[] args){
+		
+		int[][]map = {{0,0,1},{1,1,1},{1,1,3}};
+		
+		MyTools tool = new MyTools();
+		tool.printArr(map);
+	}	
+}
+class MyTools{
+	public void printArr(int[][]map) {
+		for(int i=0;i<map.length;i++) {
+			for(int j=0;j<map[i].length;j++) {
+				System.out.println(map[i][j]+"");
+			}
+			System.out.println();
+		}
+	}
+}
