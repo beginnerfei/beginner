f2311df8581b9a25382c51341d1677586f092e5c
beginnerfei <beginnerfei@qq.com>
2023-03-22 19:34:37 +0800

    方法传参机制2

---
+++ MerhodParametero2.java
@@ -0,0 +1,24 @@
+public class MerhodParametero2{
+	public static void main(String[] args) {
+		
+		//B b = new B();
+		int[] arr = {1,2,3};
+		//b.test100(arr);
+		
+		System.out.println("test100的arr数组");
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+		System.out.println();
+	}
+}
+/*class B {
+	public void test100(int[] arr) {
+		arr[0] = 200;
+		System.out.println("test100的arr数组");
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+		System.out.println();
+	}
+}*/
