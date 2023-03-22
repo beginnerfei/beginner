47e14c029b6f2475f660719cacf99f37874939c5
beginnerfei <beginnerfei@qq.com>
2023-03-22 16:20:54 +0800

    0209方法练习题1

---
+++ MethodExercise01.java
@@ -0,0 +1,28 @@
+public class MethodExercise01{
+	public static void main(String[] args){
+		
+		/*
+		 * AA a = new AA();
+		 
+		
+		
+		if(a.isOdd(4)) {
+			System.out.println("奇数");
+	    }else {
+	    	System.out.println("偶数");
+	    }*/
+    }
+}
+/* class AA{
+	public boolean isOdd(int num) {
+		return num%2!=0;
+	}
+	public void print(int row,int col,char c) {
+		for(int i=0;i<row;i++) {
+			for(int j=0;j<col;j++) {
+				System.out.print(c);
+			}
+			System.out.println();换行
+		}
+	}
+}*/
