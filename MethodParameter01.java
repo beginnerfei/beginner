a898cd55af691abcb718f8d3a2d811303bed64c4
beginnerfei <beginnerfei@qq.com>
2023-03-22 18:42:19 +0800

    方法传参机制1

---
+++ MethodParameter01.java
@@ -0,0 +1,20 @@
+public class MethodParameter01{
+	public static void main(String[] args){
+		
+		int a = 10;
+		int b = 20;
+		//AA abj = new AA();
+		//abj.swap(a,b);
+		System.out.println("a="+a+"b="+b);
+	}
+}
+
+/*class AA{
+	public void swap(int a,int b) {
+		System.out.println("交换前a="+a+"b="+b);
+		int tmp = a;
+		a = b;
+		b = tmp;//交换
+		System.out.println("交换后a="+a+"b="+b);
+	}
+}*/
