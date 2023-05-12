6384e55ef74a44874a0b04aa4c4c47967929fd82
beginnerfei <beginnerfei@qq.com>
2023-05-12 15:39:22 +0800

    模版0272

---
+++ TestTemplate.java
@@ -0,0 +1,10 @@
+package beginner2;
+
+public class TestTemplate {
+	public static void main(String[] args) { //main alt+/
+		System.out.println();//syso alt+/
+		for (int i = 0; i < args.length; i++) {//for alt+/ 	第二个
+			
+		}
+	}
+}
