997938e023afb97abc76a1f21d94a234161e8fe4
beginnerfei <beginnerfei@qq.com>
2023-05-16 18:01:09 +0800

    语法：常量

---
+++ ConstantDemo.java
@@ -0,0 +1,18 @@
+package com.itheima.constant;
+
+public class ConstantDemo {
+	
+	public static final String SCHOOL_NAME ="黑马";
+	public static final String USER_NAME ="itheima";
+	public static final String PASS_WORD ="123456";
+	
+	public static void main(String[] args) {
+		System.out.println(SCHOOL_NAME);
+		System.out.println(SCHOOL_NAME);
+		System.out.println(SCHOOL_NAME);//宏替换
+		
+		
+	}
+
+
+}
