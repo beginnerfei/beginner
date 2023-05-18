044a09858147e81c9c9281d0fefb0ecaf99e9b0c
beginnerfei <beginnerfei@qq.com>
2023-05-18 18:01:42 +0800

    常用API-Math

---
+++ MathDemo.java
@@ -0,0 +1,31 @@
+package com.itheima.math;
+
+public class MathDemo {
+
+	public static void main(String[] args) {
+		
+		
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println(Math.abs(10));
+		System.out.println(Math.abs(-10.3));
+		
+		System.out.println(Math.ceil(4.000001));
+		System.out.println(Math.ceil(4.0));
+		
+		System.out.println(Math.floor(3.99999));
+		System.out.println(Math.floor(3.0));
+		
+		System.out.println(Math.pow(2, 3));
+		
+		System.out.println(Math.round(4.44444));
+		System.out.println(Math.round(4.5001));
+		
+		System.out.println(Math.random());
+		
+		//int data = (int)(Math.random()*7+3);
+
+	}
+
+}
