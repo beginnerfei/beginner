372fddb431d8efa89e9faa351f2283d169f73f3f
beginnerfei <beginnerfei@qq.com>
2023-05-31 19:08:40 +0800

    汉诺塔

---
+++ HannoTower.java
@@ -0,0 +1,27 @@
+package com.hsp.migong;
+
+public class HannoTower {
+
+	public static void main(String[] args) {
+
+		Tower tower = new Tower();
+		tower.move(64, 'A', 'B', 'C');
+	}
+}
+
+class Tower {
+
+	public void move(int num, char a, char b, char c) {
+
+		if (num == 1) {
+			System.out.println(a + "->" + c);
+		} else {
+
+			move(num - 1, a, c, b);
+
+			System.out.println(a + "->" + c);
+
+			move(num - 1, b, a, c);
+		}
+	}
+}
