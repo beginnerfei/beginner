da211ce14b11d56f2c974628d11cb3681e5d0a29
beginnerfei <beginnerfei@qq.com>
2023-06-01 14:53:16 +0800

    方法重写

---
+++ override/Dog.java
@@ -0,0 +1,24 @@
+package com.hsp.override;
+
+public class Dog extends Animal {
+
+	public void cry() {
+		System.out.println("小狗汪汪叫....");
+	}
+
+	public String m1() {
+		return null;
+	}
+
+	public void eat() {
+
+	}
+}
+
+class AAA {
+
+}
+
+class BBB extends AAA {
+
+}
