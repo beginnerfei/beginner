65927c6a632a6c9832eaf4364731cebaeaebb26e
beginnerfei <beginnerfei@qq.com>
2023-06-02 16:00:28 +0800

    零钱通OOP版

---
+++ smallchangeoop/SmallChangeSysApp.java
@@ -0,0 +1,11 @@
+package com.hsp.smallchangeoop;
+
+public class SmallChangeSysApp {
+
+	public static void main(String[] args) {
+		System.out.println("======hello公司======");
+		new SmallChangeSysOOP().mainMenu();
+		
+		//直接调用SmallChangeSysOOP 对象，显示主菜单
+	}
+}
