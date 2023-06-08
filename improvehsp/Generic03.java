9cec69f6ceba6b366c8e9c193ad0affb453d9b9d
beginnerfei <beginnerfei@qq.com>
2023-06-09 07:53:21 +0800

    泛型

---
+++ improvehsp/Generic03.java
@@ -0,0 +1,28 @@
+package com.hsp.improvehsp;
+
+public class Generic03 {
+	public static void main(String[] args) {
+
+		Person<String> person = new Person<String>("韩顺平");
+		person.show(); 
+
+		Person<Integer> person2 = new Person<Integer>(100);
+		person2.show();
+	}
+}
+
+class Person<E> {
+	E s;
+
+	public Person(E s) {
+		this.s = s;
+	}
+
+	public E f() {
+		return s;
+	}
+
+	public void show() {
+		System.out.println(s.getClass());
+	}
+}
