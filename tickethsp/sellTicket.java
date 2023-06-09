a49f9fcbde23d187fbe2b00f0552d2227650b004
beginnerfei <beginnerfei@qq.com>
2023-06-09 18:25:20 +0800

    多线程售票问题

---
+++ tickethsp/sellTicket.java
@@ -0,0 +1,73 @@
+package com.hsp.tickethsp;
+
+public class sellTicket {
+
+	public static void main(String[] args) {
+
+		// 测试
+//	        SellTicket01 sellTicket01 = new SellTicket01();
+//	        SellTicket01 sellTicket02 = new SellTicket01();
+//	        SellTicket01 sellTicket03 = new SellTicket01();
+//	        sellTicket01.start();//启动售票线程
+//	        sellTicket02.start();//启动售票线程
+//	        sellTicket03.start();//启动售票线程
+
+		System.out.println("======使用实现接口方式来售票=======");
+		SellTicket02 sellTicket02 = new SellTicket02();
+
+		new Thread(sellTicket02).start();
+		new Thread(sellTicket02).start();
+		new Thread(sellTicket02).start();
+
+	}
+}
+
+class SellTicket01 extends Thread {
+
+	private static int ticketNum = 100;
+
+	@Override
+	public void run() {
+		while (true) {
+
+			if (ticketNum <= 0) {
+				System.out.println("售票结束");
+				break;
+			}
+
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			System.out.println("窗口 " + Thread.currentThread().getName() + " 售出一张票" + " 剩余票数=" + (--ticketNum));
+
+		}
+	}
+}
+
+class SellTicket02 implements Runnable {
+	private int ticketNum = 100;
+
+	@Override
+	public void run() {
+		while (true) {
+
+			if (ticketNum <= 0) {
+				System.out.println("售票结束");
+				break;
+			}
+
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			System.out.println("窗口 " + Thread.currentThread().getName() + " 售出一张票" + " 剩余票数=" + (--ticketNum));
+																												
+
+		}
+	}
+}
