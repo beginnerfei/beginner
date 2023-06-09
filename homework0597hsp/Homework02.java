071984d80da1557e9839ae94b3351aa2cdef8f78
beginnerfei <beginnerfei@qq.com>
2023-06-10 06:55:56 +0800

    0597线程家庭作业12

---
+++ homework0597hsp/Homework02.java
@@ -0,0 +1,39 @@
+package com.hsp.homework0597hsp;
+
+public class Homework02 {
+
+	public static void main(String[] args) {
+		T t = new T();
+		Thread thread1 = new Thread(t);
+		thread1.setName("t1");
+		Thread thread2 = new Thread(t);
+		thread2.setName("t2");
+		thread1.start();
+		thread2.start();
+	}
+}
+
+class T implements Runnable {
+	private int money = 10000;
+
+	@Override
+	public void run() {
+		while (true) {
+
+			synchronized (this) {
+				if (money < 1000) {	// 判断余额是否够
+					System.out.println("余额不足");
+					break;
+				}
+				money -= 1000;
+				System.out.println(Thread.currentThread().getName() + " 取出了1000 当前余额=" + money);
+			}
+
+			try {// 休眠1s
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
