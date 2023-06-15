f07f59105186c50de40cd69f73e7bba2bf6a4ec3
beginnerfei <beginnerfei@qq.com>
2023-06-15 10:06:58 +0800

    动态和静态加载0719

---
+++ classload0719hsp/Test.java
@@ -0,0 +1,12 @@
+package com.hsp.reflection.classload0719hsp;
+
+import com.hsp.hspedu0710hsp.Cat;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Cat cat = new Cat();
+
+	}
+}
